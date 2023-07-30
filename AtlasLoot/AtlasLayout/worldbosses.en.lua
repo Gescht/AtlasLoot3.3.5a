f437767f282566abd8424cb4119476c586e3b3a3
Gescht <tobias.alfons.tampe@gmail.com>
2023-07-31 01:09:09 +0200

    add content

---
+++ AtlasLoot/AtlasLayout/worldbosses.en.lua
@@ -0,0 +1,65 @@
+--[[
+worldbosses.en.lua
+This file defines the loot buttons in the Atlas interface for the WorldBosses maps.
+]]
+
+AtlasLootWBBossButtons = {
+
+    FourDragons = {
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "DLethon";
+        "DEmeriss";
+        "DTaerar";
+        "DYsondre";
+        };
+
+    Azuregos = {
+        "WorldBossesClassic";
+        };
+    Kazzak = {
+        "Kazzak60";
+        };
+    DoomLordKazzak = {
+        "WorldBossesBC";
+        };
+
+    Doomwalker = {
+        "WorldBossesBC";
+        };
+
+    Skettis = {
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "Terokk";
+        "";
+        "";
+        "SkettisTalonpriestIshaal";
+        "";
+        "";
+        "SkettisHazziksPackage";
+        "";
+        "";
+        "DarkscreecherAkkarai";
+        "GezzaraktheHuntress";
+        "Karrog";
+        "VakkiztheWindrager";
+        };
+
+    };
